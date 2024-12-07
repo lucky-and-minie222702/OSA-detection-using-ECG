3769a291ba2f171199239aed61e6fa7b37372544
techn0z <minhtrietamoled@gmail.com>
2024-12-07 21:14:16 +0700

    update

--- src/model_SpO2.py
+++ src/model_SpO2.py
@@ -11,7 +11,7 @@ def create_model(name: str):
     x = layers.Conv1D(filters=32, kernel_size=3, kernel_regularizer=reg.L2())(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
-    x = layers.MaxPool1D(pool_size=2)(x)
+    x = layers.MaxPool1D(pool_size=4)(x)
     x = layers.Conv1D(filters=64, kernel_size=3, kernel_regularizer=reg.L2())(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
