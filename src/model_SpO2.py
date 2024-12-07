344e903d0f679a99f6b5bb2f8aa5c5c571a0b10f
techn0z <minhtrietamoled@gmail.com>
2024-12-07 12:23:38 +0700

    update

--- src/model_SpO2.py
+++ src/model_SpO2.py
@@ -18,7 +18,7 @@ def create_model(name: str):
     x = layers.GlobalMaxPool1D()(x)
     x = layers.Flatten()(x)
     out = layers.Dense(128, activation="relu")(x)
-    out = layers.Dropout(rate=0.25)(out)
+    out = layers.Dropout(rate=0.5)(out)
     out = layers.Dense(1, activation="sigmoid")(out)
     
     model = Model(
