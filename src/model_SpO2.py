11025024916559038ff57f4a7f059a0da787344c
techn0z <minhtrietamoled@gmail.com>
2024-12-10 09:19:35 +0700

    update

--- src/model_SpO2.py
+++ src/model_SpO2.py
@@ -7,7 +7,7 @@ import os
 def create_model(name: str):
     inp = layers.Input(shape=(None, 1))
     x = layers.Normalization()(inp)
-    x = layers.Conv1D(filters=16, kernel_size=3, kernel_regularizer=reg.L2(), padding="same")(x)
+    x = layers.Conv1D(filters=16, kernel_size=3, kernel_regularizer=reg.L2(), padding="same", name = "Inital")(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(negative_slope=0.3)(x)
     x = layers.MaxPool1D(pool_size=2)(x)
@@ -95,7 +95,8 @@ cb_weight_memory = WeightMemoryMechanism(
     patience = 3
 )
 cb_forget = DynamicWeightSparsification(
-    sparsity_target = 0.01
+    sparsity_target = 0.01,
+    layer_names = ["Initial"]
 )
 
 if not "id" in sys.argv:
