8e35d67a8087b387caca7b9a179dc2a1288d633d
techn0z <minhtrietamoled@gmail.com>
2024-12-13 16:58:23 +0700

    seccond

--- generate.sh
+++ generate.sh
@@ -1,5 +1,4 @@
 python src/preprocess.py
 python src/patients_action.py merge ECG SpO2
 python src/patients_action.py augment ECG SpO2
-python src/patients_action.py save_stats
-python src/patients_action.py split_dataset ECG SpO2 stats
+python src/patients_action.py split_dataset ECG SpO2
