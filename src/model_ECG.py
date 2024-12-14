c0f9ea0cc2a61ea89272cfe566f57ac322f0071c
techn0z <minhtrietamoled@gmail.com>
2024-12-14 21:37:39 +0700

    update

--- src/model_ECG.py
+++ src/model_ECG.py
@@ -9,27 +9,20 @@ def create_model(name: str):
     inp = layers.Input(shape=(None, 1))
     conv = layers.Normalization()(inp)
     
-    conv = layers.Conv1D(filters=32, kernel_size=5, kernel_regularizer=reg.L2(), padding="same")(conv)
+    conv = layers.Conv1D(filters=16, kernel_size=5, kernel_regularizer=reg.L2(), padding="same")(conv)
     conv = layers.BatchNormalization()(conv)
     conv = layers.Activation("relu")(conv)
     conv = layers.Conv1D(filters=32, kernel_size=5, kernel_regularizer=reg.L2(), padding="same")(conv)
-    conv = layers.BatchNormalization()(conv)
-    conv = layers.Activation("relu")(conv)
-    
-    conv = SEBlock(reduction_ratio=2)(conv)
-    
-    conv = layers.Conv1D(filters=64, kernel_size=3, kernel_regularizer=reg.L2(), padding="same")(conv)
     conv = layers.BatchNormalization()(conv)
     conv = layers.Activation("relu")(conv)
     conv = layers.Conv1D(filters=64, kernel_size=3, kernel_regularizer=reg.L2(), padding="same")(conv)
     conv = layers.BatchNormalization()(conv)
     conv = layers.Activation("relu")(conv)
     
-    att = SEBlock(reduction_ratio=4)(conv)
+    # att = SEBlock(reduction_ratio=4)(conv)
 
-    flat = layers.GlobalMaxPool1D()(att)
+    flat = layers.GlobalMaxPool1D()(conv)
     flat = layers.Flatten()(flat)
-    flat = layers.Dropout(rate=0.5)(flat)
     out = layers.Dense(1, activation="sigmoid")(flat)
     
     model = Model(
