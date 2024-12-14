7603589eb83ee80f5d2a9816f474db3a572748dd
techn0z <minhtrietamoled@gmail.com>
2024-12-14 15:55:47 +0700

    update

--- src/model_ECG.py
+++ src/model_ECG.py
@@ -48,7 +48,7 @@ if "epochs" in sys.argv:
     epochs = int(sys.argv[sys.argv.index("epochs")+1])
 else:
     epochs = int(input("Please provide a valid number of epochs: "))
-batch_size = 256
+batch_size = 512
 es_ep = 50
 
 print("Creating model architecture...")
